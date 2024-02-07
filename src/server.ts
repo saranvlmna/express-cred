1a41ba910e56fd4c295d0b720b7e71a8fb2f2e17
Saranjith <saranjith@metrictreelabs.com>
2024-02-07 12:48:59 +0530

    init app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
-import express, { Express, Request, Response } from "express";
-
-const app: Express = express();
+import { app } from "./app";
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${4578}`);
