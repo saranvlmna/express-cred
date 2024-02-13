318e64f5f075127879dab0b13cb6b4f3aa24fcf5
Saranjith <saranjith@metrictreelabs.com>
2024-02-13 11:55:50 +0530

    status code added

--- src/controller/server.ts
+++ src/controller/server.ts
@@ -1,10 +1,20 @@
+import {
+  StatusCodes
+} from "http-status-codes";
 import { ServerService } from "../service/server";
 const serverService = new ServerService();
 export class ServerController {
-  constructor() {}
+  constructor() { }
 
   async heyServer(req: any, res: any) {
-    const response = await serverService.heyServer();
-    res.send(response);
+    try {
+      const response = await serverService.heyServer();
+      res.status(StatusCodes.OK).send({
+        response,
+      });
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 }
