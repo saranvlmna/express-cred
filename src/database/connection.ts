073bffbae8d4f4666256014398e883d3792b6b08
Saranjith <saranjith@metrictreelabs.com>
2024-02-23 16:08:09 +0530

    sequelizse connection added

---
+++ src/database/connection.ts
@@ -0,0 +1,36 @@
+const DataTypes = require("sequelize");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("directions", "postgres", "vlmna", {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false,
+});
+
+sequelize.authenticate().then(() => {
+    console.log("Connection has been established successfully.");
+});
+
+const User = sequelize.define("user", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    age: {
+        type: DataTypes.STRING,
+    },
+    place: {
+        type: DataTypes.STRING,
+    },
+    address: {
+        type: DataTypes.STRING,
+    },
+});
+
+sequelize.sync();
+
+export { User };
